96f9876639708a6ccb1402642aeca422164dfa6e
Sidney Alcantara <sidney@sidney.me>
2022-05-31 11:59:24 +1000

    Revert "attempt fix netlify pnpm"
    This reverts commit a9c9ea751c7083dd0ca50f014bc1ec123d5313be.

--- .netlify/netlify-plugin-pnpm/index.js
+++ .netlify/netlify-plugin-pnpm/index.js
@@ -2,10 +2,8 @@
 module.exports = {
   onPreBuild: async ({ utils: { build, run } }) => {
     try {
-      await run.command("npm install -g pnpm --force");
-      await run.command(
-        "pnpm install --frozen-lockfile=false -r --shamefully-hoist --store=node_modules/.pnpm-store"
-      );
+      await run.command("npm install -g pnpm");
+      await run.command("pnpm install --frozen-lockfile=false");
     } catch (error) {
       return build.failBuild(error);
     }
