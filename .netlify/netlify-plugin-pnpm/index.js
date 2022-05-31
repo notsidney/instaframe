a9c9ea751c7083dd0ca50f014bc1ec123d5313be
Sidney Alcantara <sidney@sidney.me>
2022-05-31 11:43:16 +1000

    attempt fix netlify pnpm

--- .netlify/netlify-plugin-pnpm/index.js
+++ .netlify/netlify-plugin-pnpm/index.js
@@ -2,8 +2,10 @@
 module.exports = {
   onPreBuild: async ({ utils: { build, run } }) => {
     try {
-      await run.command("npm install -g pnpm");
-      await run.command("pnpm install --frozen-lockfile=false");
+      await run.command("npm install -g pnpm --force");
+      await run.command(
+        "pnpm install --frozen-lockfile=false -r --shamefully-hoist --store=node_modules/.pnpm-store"
+      );
     } catch (error) {
       return build.failBuild(error);
     }
